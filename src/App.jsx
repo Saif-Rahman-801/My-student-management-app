872e605af5c0743332ca65c4394fb0f865b4d5f6
Saif-Rahman-801 <saifurs102@gmail.com>
2023-07-21 14:35:08 +0600

    layout and createStudentHandler created

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,89 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [studentName, setStudentName] = useState("");
+  const [students, setStudents] = useState([]);
+  const [editMode, setEditMode] = useState(false);
+  const [editableTodo, setEditableTodo] = useState(null);
+
+  const createStudenthandler = () => {
+    if (studentName) {
+      const newStudent = {
+        id: Date.now(),
+        name: studentName,
+      };
+      setStudents([...students, newStudent]);
+      setStudentName("")
+    } else {
+      alert("Please provide valid info");
+    }
+  };
+
+  const editStudentHandler = (id) => {};
+
+  const deleteStudentHandler = (id) => {};
+
+  const updateStudentHandler = (id) => {};
+
+  const presentHandler = (id) => {};
+
+  const absentHandler = (id) => {};
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <form>
+        <input
+          type="text"
+          name="text"
+          id="text"
+          placeholder="Enter a valid name"
+          value={studentName}
+          onChange={(e) => setStudentName(e.target.value)}
+        />
+        <button>Add Student</button>
+      </form>
+      <div className="students-section">
+        <div className="all-student">
+          <ul>
+            {students.map((student) => (
+              <li>
+                <span>{student.name}</span>
+                <button>Edit</button>
+                <button>Delete</button>
+                <button>Present</button>
+                <button>absent</button>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="present-student">
+          <ul>
+            {students
+              .filter((item) => item.isPresent === true)
+              .map((student) => (
+                <li>
+                  <span>{student.name}</span>
+                  <button>Accidentally added</button>
+                </li>
+              ))}
+          </ul>
+        </div>
+        <div className="absent-studen">
+          <ul>
+            {students
+              .filter((item) => item.isPresent === true)
+              .map((student) => (
+                <li>
+                  <span>{student.name}</span>
+                  <button>Accidentally added</button>
+                </li>
+              ))}
+          </ul>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
